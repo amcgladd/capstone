04327f546a20067b8ebce4c9b531108aeb6a0f16
AJ Mcgladdery <amcgladd@gmail.com>
2018-12-18 11:10:51 -0800

    add assets for portal components

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -66,6 +66,20 @@ export default class HelloWorldSceneAR extends Component {
               arShadowReceiver={true} />
 
         </ViroARImageMarker>
+
+//borrowed from tutorial
+        <ViroPortalScene passable={true} dragType="FixedDistance" onDrag={()=>{}}>
+                  <ViroPortal position={[0, 0, -1]} scale={[.1, .1, .1]}>
+                    <Viro3DObject source={require('./portal_res/portal_ship/portal_ship.vrx')}
+                      resources={[require('./portal_res/portal_ship/portal_ship_diffuse.png'),
+                                  require('./portal_res/portal_ship/portal_ship_normal.png'),
+                                  require('./portal_res/portal_ship/portal_ship_specular.png')]}
+                      type="VRX"/>
+                  </ViroPortal>
+                  <Viro360Image source={require("./portal_res/360_island.jpg")} />
+                </ViroPortalScene>
+
+
       </ViroARScene>
     );
   }
